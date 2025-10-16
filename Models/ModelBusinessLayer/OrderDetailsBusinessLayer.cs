25e216577747fb3451ff25272b15e63b47a31565
Ojas Agrawal <Ojas Agrawal@FT-LAPTOP-7>
2025-10-16 15:06:32 -0400

    Can now add pizzas to cart and view cart

--- Models/ModelBusinessLayer/OrderDetailsBusinessLayer.cs
+++ Models/ModelBusinessLayer/OrderDetailsBusinessLayer.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.Identity.Client;
+using PizzaStore.Data;
 using System.Data;
 
 namespace PizzaStore.Models.ModelBusinessLayer
@@ -8,7 +9,7 @@ namespace PizzaStore.Models.ModelBusinessLayer
     {
         //Add a new item order to a cart to be later added to an order to be added to a customer
         public void AddItem(Product product, int quantity)
-        {
+        {   
             string connectionstring = "Server=.;Database=Pizza;Trusted_Connection=True;TrustServerCertificate=True;";
 
             using(SqlConnection con = new SqlConnection(connectionstring))
@@ -25,10 +26,6 @@ namespace PizzaStore.Models.ModelBusinessLayer
                 paramProductId.ParameterName = "@ProductId";
                 paramProductId.Value = product.Id;
                 cmd.Parameters.Add(paramProductId);
-
-                SqlParameter paramProduct = new SqlParameter();
-                paramProduct.ParameterName = "@Product";
-                paramProduct.Value = product;
 
                 con.Open();
                 cmd.ExecuteNonQuery();
