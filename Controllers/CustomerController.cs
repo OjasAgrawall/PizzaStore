00f3b57b34eac95fcce381559fe3b3c92ec50a53
Ojas Agrawal <Ojas Agrawal@FT-LAPTOP-7>
2025-10-17 15:15:16 -0400

    can now create new customers

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,16 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PizzaStore.Data;
+using PizzaStore.Models;
+using PizzaStore.Models.ModelBusinessLayer;
 
 namespace PizzaStore.Controllers
 {
     public class CustomerController : Controller
     {
-        public IActionResult LogIn()
+
+        public IActionResult Login()
         {
             return View();
         }
+
+        [HttpGet]
         public IActionResult SignUp()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(Customer customer)
+        {
+            CustomerBusinessLayer customerBusinessLayer = new CustomerBusinessLayer();
+            customerBusinessLayer.AddCustomer(customer);
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ Models/ModelBusinessLayer/CustomerBusinessLayer.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace PizzaStore.Models.ModelBusinessLayer
+{
+    public class CustomerBusinessLayer
+    {
+        public void AddCustomer(Customer customer)
+        {
+            string connectionstring = "Server=.;Database=Pizza;Trusted_Connection=True;TrustServerCertificate=True;";
+
+            using (SqlConnection con = new SqlConnection(connectionstring))
+            {
+                SqlCommand cmd = new SqlCommand("spAddCustomer", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramFName = new SqlParameter();
+                paramFName.ParameterName = "@FName";
+                paramFName.Value = customer.FirstName;
+                cmd.Parameters.Add(paramFName);
+
+                SqlParameter paramLName = new SqlParameter();
+                paramLName.ParameterName = "@LName";
+                paramLName.Value = customer.LastName;
+                cmd.Parameters.Add(paramLName);
+
+                SqlParameter paramEmail = new SqlParameter();
+                paramEmail.ParameterName = "@Email";
+                paramEmail.Value = customer.Email;
+                cmd.Parameters.Add(paramEmail);
+
+                SqlParameter paramPassword = new SqlParameter();
+                paramPassword.ParameterName = "@Password";
+                paramPassword.Value = customer.Password;
+                cmd.Parameters.Add(paramPassword);
+
+                SqlParameter paramAddress = new SqlParameter();
+                paramAddress.ParameterName = "@Address";
+                paramAddress.Value = customer.Address;
+                cmd.Parameters.Add(paramAddress);
+
+                SqlParameter paramPhone = new SqlParameter();
+                paramPhone.ParameterName = "@Phone";
+                paramPhone.Value = customer.Phone;
+                cmd.Parameters.Add(paramPhone);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
